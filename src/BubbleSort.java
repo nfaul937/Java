a9ccd2cdd5fde3fd31071ab373c0c41fc920908e
Nicholas <Nicholas@192.168.20.186>
2017-03-10 20:54:04 -0600

    Fixed the input mismatch exception

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,27 +1,31 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class BubbleSort {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
         String filename = "C:\\Users\\Nicholas\\Documents\\NetBeansProjects\\theNumbers.txt";
         
-        Scanner fileinput = new Scanner(filename);
+        Scanner fileinput = new Scanner(new File (filename));
         
         int[] data = new int[10];
         int temp;
         
-        System.out.println(data.length);
+        //Print out length of file
+        System.out.println(data.length + "\n");
         int j = 0;
-        while (fileinput.hasNextLine()) {
-            data[j] = fileinput.nextInt();
+        
+        while (fileinput.hasNextInt()) {
+            int num = fileinput.nextInt();
+            data[j] = num;
             j++;
         }
         
-        System.out.println(j);
         for (int i = 0; i < data.length - 1; i++) {
             
             for (int k = 1; k < data.length - i; k++) {
